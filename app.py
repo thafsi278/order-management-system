8a3587e94477071b4d93fe727fee31a58a0cbf96
thafsi278 <thafsi.ahbab@hotmail.co.uk>
2025-01-10 03:02:09 +0000

    updating app comments

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from views import views
 
+# Function that initializes flask application
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(views, url_prefix="/")
@@ -10,4 +11,4 @@ def create_app():
     return app
 
 if __name__ == '__main__':
-    create_app()
+    create_app() # Calling function
